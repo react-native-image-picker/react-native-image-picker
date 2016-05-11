c76911a9054bc1644d5ce3f549d20d7cf1f1b22a
yfuks <yfuks@student.42.fr>
2016-05-11 15:40:09 +0200

    [Android] add path to video info

--- android/src/main/java/com/imagepicker/ImagePickerModule.java
+++ android/src/main/java/com/imagepicker/ImagePickerModule.java
@@ -336,10 +336,12 @@ public class ImagePickerModule extends ReactContextBaseJavaModule implements Act
         break;
       case REQUEST_LAUNCH_VIDEO_LIBRARY:
         response.putString("uri", data.getData().toString());
+        response.putString("path", getRealPathFromURI(data.getData()));
         mCallback.invoke(response);
         return;
       case REQUEST_LAUNCH_VIDEO_CAPTURE:
         response.putString("uri", data.getData().toString());
+        response.putString("path", getRealPathFromURI(data.getData()));
         mCallback.invoke(response);
         return;
       default:
