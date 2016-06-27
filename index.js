32ee302a51d1a6e9eeb5ced90a04dc0e537be949
Mark Vayngrib <mark@tradle.io>
2016-06-27 13:24:45 -0400

    set default opts for showImagePicker from javascript

--- index.js
+++ index.js
@@ -1,5 +1,29 @@
 'use strict'
 
-var { NativeModules } = require('react-native');
+const { NativeModules } = require('react-native');
+const { ImagePickerManager } = NativeModules
+const SHOW_PICKER_DEFAULT_OPTS = {
+  title: 'Select a Photo',
+  cancelButtonTitle: 'Cancel',
+  takePhotoButtonTitle: 'Take Photo…',
+  chooseFromLibraryButtonTitle: 'Choose from Library…',
+  quality : 0.2, // 1.0 best to 0.0 worst
+  allowsEditing : false
+}
 
-module.exports = NativeModules.ImagePickerManager;
+module.exports = {
+  ...ImagePickerManager,
+  showImagePicker: function showImagePicker (opts, callback) {
+    if (typeof opts === 'function') {
+      callback = opts
+      opts = null
+    }
+
+    opts = opts || {}
+    if ('cancelButtonTitle' in opts && !opts.cancelButtonTitle) {
+      throw new Error('empty cancel button not allowed')
+    }
+
+    return ImagePickerManager.showImagePicker({ ...SHOW_PICKER_DEFAULT_OPTS, ...opts }, callback)
+  }
+}
