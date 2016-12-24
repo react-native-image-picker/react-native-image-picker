e4dc969bf69ada0dca73630de9fc379bdb3c3466
Kane Rogers <kane.m.rogers@gmail.com>
2016-12-24 18:11:54 +1100

    Update ImagePickerActivityEventListener to use newer React Native API.

--- android/src/main/java/com/imagepicker/ImagePickerActivityEventListener.java
+++ android/src/main/java/com/imagepicker/ImagePickerActivityEventListener.java
@@ -3,11 +3,11 @@ package com.imagepicker;
 import android.app.Activity;
 import android.content.Intent;
 
-import com.facebook.react.bridge.BaseActivityEventListener;
+import com.facebook.react.bridge.ActivityEventListener;
 import com.facebook.react.bridge.ReactApplicationContext;
 
 
-public class ImagePickerActivityEventListener extends BaseActivityEventListener {
+public class ImagePickerActivityEventListener implements ActivityEventListener {
 
     private ActivityResultInterface mCallback;
 
