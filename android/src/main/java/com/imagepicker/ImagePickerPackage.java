33892368cde41b7041bf1e36693b0d9cf99e027a
Steffen Forkmann <sforkmann@gmail.com>
2017-07-28 12:20:36 +0200

    Remove deprecated method for RN 0.47 compat

--- android/src/main/java/com/imagepicker/ImagePickerPackage.java
+++ android/src/main/java/com/imagepicker/ImagePickerPackage.java
@@ -32,11 +32,6 @@ public class ImagePickerPackage implements ReactPackage {
   }
 
   @Override
-  public List<Class<? extends JavaScriptModule>> createJSModules() {
-    return Collections.emptyList();
-  }
-
-  @Override
   public List<ViewManager> createViewManagers(ReactApplicationContext reactContext) {
     return Collections.emptyList();
   }
