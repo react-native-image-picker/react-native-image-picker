7bf977ab4f4d3e0c2a3e561e94435028f5a58773
Craig P Jolicoeur <cpjolicoeur@gmail.com>
2018-05-31 11:21:21 -0400

    Update package.json to point to mojotech version
    * also updated version number

--- index.d.ts
+++ index.d.ts
@@ -1,62 +1,59 @@
-declare module "react-native-image-picker" {
+declare module '@mojotech/react-native-image-picker' {
+  interface Response {
+    customButton: string;
+    didCancel: boolean;
+    error: string;
+    data: string;
+    uri: string;
+    origURL?: string;
+    isVertical: boolean;
+    width: number;
+    height: number;
+    fileSize: number;
+    type?: string;
+    fileName?: string;
+    path?: string;
+    latitude?: number;
+    longitude?: number;
+    timestamp?: string;
+  }
 
-    interface Response {
-        customButton: string;
-        didCancel: boolean;
-        error: string;
-        data: string;
-        uri: string;
-        origURL?: string;
-        isVertical: boolean;
-        width: number;
-        height: number;
-        fileSize: number;
-        type?: string;
-        fileName?: string;
-        path?: string;
-        latitude?: number;
-        longitude?: number;
-        timestamp?: string;
-    }
+  interface CustomButtonOptions {
+    name?: string;
+    title?: string;
+  }
 
-    interface CustomButtonOptions {
-        name?: string;
-        title?: string;
-    }
+  interface Options {
+    title?: string;
+    cancelButtonTitle?: string;
+    takePhotoButtonTitle?: string;
+    chooseFromLibraryButtonTitle?: string;
+    customButtons?: Array<CustomButtonOptions>;
+    cameraType?: 'front' | 'back';
+    mediaType?: 'photo' | 'video' | 'mixed';
+    maxWidth?: number;
+    maxHeight?: number;
+    quality?: number;
+    videoQuality?: 'low' | 'medium' | 'high';
+    durationLimit?: number;
+    rotation?: number;
+    allowsEditing?: boolean;
+    noData?: boolean;
+    storageOptions?: StorageOptions;
+  }
 
-    interface Options {
-        title?: string;
-        cancelButtonTitle?: string;
-        takePhotoButtonTitle?: string;
-        chooseFromLibraryButtonTitle?: string;
-        customButtons?: Array<CustomButtonOptions>;
-        cameraType?: 'front' | 'back';
-        mediaType?: 'photo' | 'video' | 'mixed';
-        maxWidth?: number;
-        maxHeight?: number;
-        quality?: number;
-        videoQuality?: 'low' | 'medium' | 'high';
-        durationLimit?: number;
-        rotation?: number;
-        allowsEditing?: boolean;
-        noData?: boolean;
-        storageOptions?: StorageOptions;
-    }
+  interface StorageOptions {
+    skipBackup?: boolean;
+    path?: string;
+    cameraRoll?: boolean;
+    waitUntilSaved?: boolean;
+  }
 
-    interface StorageOptions {
-        skipBackup?: boolean;
-        path?: string;
-        cameraRoll?: boolean;
-        waitUntilSaved?: boolean;
-    }
+  class ImagePicker {
+    static showImagePicker(options: Options, callback: (response: Response) => void): void;
+    static launchCamera(options: Options, callback: (response: Response) => void): void;
+    static launchImageLibrary(options: Options, callback: (response: Response) => void): void;
+  }
 
-
-    class ImagePicker {
-        static showImagePicker(options: Options, callback: (response: Response) => void): void;
-        static launchCamera(options: Options, callback: (response: Response) => void): void;
-        static launchImageLibrary(options: Options, callback: (response: Response) => void): void;
-    }
-
-    export = ImagePicker;
-
+  export = ImagePicker;
 }
