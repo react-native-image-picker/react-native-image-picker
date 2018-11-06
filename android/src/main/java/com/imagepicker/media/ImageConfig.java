af212d88b4a4850765d94385df9eea5cd280806f
Casey Li <sometimescasey@gmail.com>
2018-11-06 18:26:02 -0500

    #972: change ImageConfig.withOriginalFile() to address testOnImmutable failure

--- android/src/main/java/com/imagepicker/media/ImageConfig.java
+++ android/src/main/java/com/imagepicker/media/ImageConfig.java
@@ -78,11 +78,13 @@ public class ImageConfig
 
     public @NonNull ImageConfig withOriginalFile(@Nullable final File original)
     {
-        //if it is a GIF file, always set quality to 100 to prevent compression
-        String extension = MimeTypeMap.getFileExtensionFromUrl(original.getAbsolutePath());
-        int quality = this.quality;
-        if(extension.contains("gif")){
-            quality = 100;
+        if (original != null) {
+            //if it is a GIF file, always set quality to 100 to prevent compression
+            String extension = MimeTypeMap.getFileExtensionFromUrl(original.getAbsolutePath());
+            int quality = this.quality;
+            if(extension.contains("gif")){
+                quality = 100;
+            }
         }
 
         return new ImageConfig(
