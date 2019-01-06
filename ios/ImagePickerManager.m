c3fa45b576f2982835a808e7a214a035ae0bb7b1
Salvatore Randazzo <sal@paperlesspost.com>
2019-01-06 04:34:04 +0100

    Resolve memory leak in iOS image picker (#835)
    * Move all alert code to main thread
    Eliminiate strong reference to UIAlertController which causes a memory leak
    
    * Delete contents.xcworkspacedata
    
    * Delete IDEWorkspaceChecks.plist

--- ios/ImagePickerManager.m
+++ ios/ImagePickerManager.m
@@ -9,7 +9,6 @@
 
 @interface ImagePickerManager ()
 
-@property (nonatomic, strong) UIAlertController *alertController;
 @property (nonatomic, strong) UIImagePickerController *picker;
 @property (nonatomic, strong) RCTResponseSenderBlock callback;
 @property (nonatomic, strong) NSDictionary *defaultOptions;
@@ -40,66 +39,67 @@ RCT_EXPORT_METHOD(showImagePicker:(NSDictionary *)options callback:(RCTResponseS
     self.callback = callback; // Save the callback so we can use it from the delegate methods
     self.options = options;
 
-    NSString *title = [self.options valueForKey:@"title"];
-    if ([title isEqual:[NSNull null]] || title.length == 0) {
-        title = nil; // A more visually appealing UIAlertControl is displayed with a nil title rather than title = @""
-    }
-    NSString *cancelTitle = [self.options valueForKey:@"cancelButtonTitle"];
-    NSString *takePhotoButtonTitle = [self.options valueForKey:@"takePhotoButtonTitle"];
-    NSString *chooseFromLibraryButtonTitle = [self.options valueForKey:@"chooseFromLibraryButtonTitle"];
+    dispatch_async(dispatch_get_main_queue(), ^{
+
+        NSString *title = [self.options valueForKey:@"title"];
+        if ([title isEqual:[NSNull null]] || title.length == 0) {
+            title = nil; // A more visually appealing UIAlertControl is displayed with a nil title rather than title = @""
+        }
+        NSString *cancelTitle = [self.options valueForKey:@"cancelButtonTitle"];
+        NSString *takePhotoButtonTitle = [self.options valueForKey:@"takePhotoButtonTitle"];
+        NSString *chooseFromLibraryButtonTitle = [self.options valueForKey:@"chooseFromLibraryButtonTitle"];
 
 
-    self.alertController = [UIAlertController alertControllerWithTitle:title message:nil preferredStyle:UIAlertControllerStyleActionSheet];
+        UIAlertController *alertController = [UIAlertController alertControllerWithTitle:title message:nil preferredStyle:UIAlertControllerStyleActionSheet];
 
-    UIAlertAction *cancelAction = [UIAlertAction actionWithTitle:cancelTitle style:UIAlertActionStyleCancel handler:^(UIAlertAction * action) {
-        self.callback(@[@{@"didCancel": @YES}]); // Return callback for 'cancel' action (if is required)
-    }];
-    [self.alertController addAction:cancelAction];
-
-    if (![takePhotoButtonTitle isEqual:[NSNull null]] && takePhotoButtonTitle.length > 0) {
-        UIAlertAction *takePhotoAction = [UIAlertAction actionWithTitle:takePhotoButtonTitle style:UIAlertActionStyleDefault handler:^(UIAlertAction * action) {
-            [self actionHandler:action];
+        UIAlertAction *cancelAction = [UIAlertAction actionWithTitle:cancelTitle style:UIAlertActionStyleCancel handler:^(UIAlertAction * action) {
+            self.callback(@[@{@"didCancel": @YES}]); // Return callback for 'cancel' action (if is required)
         }];
-        [self.alertController addAction:takePhotoAction];
-    }
-    if (![chooseFromLibraryButtonTitle isEqual:[NSNull null]] && chooseFromLibraryButtonTitle.length > 0) {
-        UIAlertAction *chooseFromLibraryAction = [UIAlertAction actionWithTitle:chooseFromLibraryButtonTitle style:UIAlertActionStyleDefault handler:^(UIAlertAction * action) {
-            [self actionHandler:action];
-        }];
-        [self.alertController addAction:chooseFromLibraryAction];
-    }
+        [alertController addAction:cancelAction];
 
-    // Add custom buttons to action sheet
-    if ([self.options objectForKey:@"customButtons"] && [[self.options objectForKey:@"customButtons"] isKindOfClass:[NSArray class]]) {
-        self.customButtons = [self.options objectForKey:@"customButtons"];
-        for (NSString *button in self.customButtons) {
-            NSString *title = [button valueForKey:@"title"];
-            UIAlertAction *customAction = [UIAlertAction actionWithTitle:title style:UIAlertActionStyleDefault handler:^(UIAlertAction * action) {
+        if (![takePhotoButtonTitle isEqual:[NSNull null]] && takePhotoButtonTitle.length > 0) {
+            UIAlertAction *takePhotoAction = [UIAlertAction actionWithTitle:takePhotoButtonTitle style:UIAlertActionStyleDefault handler:^(UIAlertAction * action) {
                 [self actionHandler:action];
             }];
-            [self.alertController addAction:customAction];
+            [alertController addAction:takePhotoAction];
         }
-    }
+        if (![chooseFromLibraryButtonTitle isEqual:[NSNull null]] && chooseFromLibraryButtonTitle.length > 0) {
+            UIAlertAction *chooseFromLibraryAction = [UIAlertAction actionWithTitle:chooseFromLibraryButtonTitle style:UIAlertActionStyleDefault handler:^(UIAlertAction * action) {
+                [self actionHandler:action];
+            }];
+            [alertController addAction:chooseFromLibraryAction];
+        }
 
-    dispatch_async(dispatch_get_main_queue(), ^{
+        // Add custom buttons to action sheet
+        if ([self.options objectForKey:@"customButtons"] && [[self.options objectForKey:@"customButtons"] isKindOfClass:[NSArray class]]) {
+            self.customButtons = [self.options objectForKey:@"customButtons"];
+            for (NSString *button in self.customButtons) {
+                NSString *title = [button valueForKey:@"title"];
+                UIAlertAction *customAction = [UIAlertAction actionWithTitle:title style:UIAlertActionStyleDefault handler:^(UIAlertAction * action) {
+                    [self actionHandler:action];
+                }];
+                [alertController addAction:customAction];
+            }
+        }
+
         UIViewController *root = RCTPresentedViewController();
 
         /* On iPad, UIAlertController presents a popover view rather than an action sheet like on iPhone. We must provide the location
         of the location to show the popover in this case. For simplicity, we'll just display it on the bottom center of the screen
         to mimic an action sheet */
-        self.alertController.popoverPresentationController.sourceView = root.view;
-        self.alertController.popoverPresentationController.sourceRect = CGRectMake(root.view.bounds.size.width / 2.0, root.view.bounds.size.height, 1.0, 1.0);
+        alertController.popoverPresentationController.sourceView = root.view;
+        alertController.popoverPresentationController.sourceRect = CGRectMake(root.view.bounds.size.width / 2.0, root.view.bounds.size.height, 1.0, 1.0);
 
         if (UI_USER_INTERFACE_IDIOM() == UIUserInterfaceIdiomPad) {
-            self.alertController.popoverPresentationController.permittedArrowDirections = 0;
-            for (id subview in self.alertController.view.subviews) {
+            alertController.popoverPresentationController.permittedArrowDirections = 0;
+            for (id subview in alertController.view.subviews) {
                 if ([subview isMemberOfClass:[UIView class]]) {
                     ((UIView *)subview).backgroundColor = [UIColor whiteColor];
                 }
             }
         }
 
-        [root presentViewController:self.alertController animated:YES completion:nil];
+        [root presentViewController:alertController animated:YES completion:nil];
     });
 }
 
