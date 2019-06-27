4a414dbb8b805150e69c144d517f95be808b82d7
OmranElmasry <OmranElmasry@gmail.com>
2019-06-27 15:58:09 +0200

    update android support library to androidx

--- android/src/main/java/com/imagepicker/utils/ButtonsHelper.java
+++ android/src/main/java/com/imagepicker/utils/ButtonsHelper.java
@@ -1,7 +1,7 @@
 package com.imagepicker.utils;
 
-import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 
 import com.facebook.react.bridge.ReadableArray;
 import com.facebook.react.bridge.ReadableMap;
@@ -29,7 +29,8 @@ public class ButtonsHelper
         }
     }
 
-    public final @Nullable Item btnCamera;
+    public final @Nullable
+    Item btnCamera;
     public final @Nullable Item btnLibrary;
     public final @Nullable Item btnCancel;
     public final List<Item> customButtons;
