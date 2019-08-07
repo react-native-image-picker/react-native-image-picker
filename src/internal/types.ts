92fd37fca7cbe7a971392f9222bd849aa858cb94
Johan Forslund <johan.f@taxicaller.com>
2019-08-07 13:46:38 +0200

    Added type and documentation

--- src/internal/types.ts
+++ src/internal/types.ts
@@ -36,6 +36,7 @@ export interface ImagePickerOptions {
   cancelButtonTitle?: string;
   takePhotoButtonTitle?: string;
   chooseFromLibraryButtonTitle?: string;
+  chooseWhichLibraryTitle?: string;
   customButtons?: ImagePickerCustomButtonOptions[];
   cameraType?: 'front' | 'back';
   mediaType?: 'photo' | 'video' | 'mixed';
