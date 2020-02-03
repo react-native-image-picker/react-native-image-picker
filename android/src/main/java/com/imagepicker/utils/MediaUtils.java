6005f36dd789cf5797b6f961f1305e007292744c
Levi <levi@yourwebfile.com>
2020-02-03 15:15:54 -0800

    allow android to save photos to private directory

--- android/src/main/java/com/imagepicker/utils/MediaUtils.java
+++ android/src/main/java/com/imagepicker/utils/MediaUtils.java
@@ -45,11 +45,35 @@ public class MediaUtils
                 .append(".jpg")
                 .toString();
 
-        final File path = ReadableMapUtils.hasAndNotNullReadableMap(options, "storageOptions")
-                && ReadableMapUtils.hasAndNotEmptyString(options.getMap("storageOptions"), "path")
-                ? new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES), options.getMap("storageOptions").getString("path"))
-                : (!forceLocal ? Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES)
-                              : reactContext.getExternalFilesDir(Environment.DIRECTORY_PICTURES));
+        // defaults to Public Pictures Directory
+        File path = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES);
+
+        if (ReadableMapUtils.hasAndNotNullReadableMap(options, "storageOptions")) 
+        {
+            final ReadableMap storageOptions = options.getMap("storageOptions");
+
+            boolean saveToPrivateDirectory = false;
+            if (storageOptions.hasKey("privateDirectory"))
+            {
+                saveToPrivateDirectory = storageOptions.getBoolean("privateDirectory");
+                if (saveToPrivateDirectory)
+                {
+                    // if privateDirectory is set then save to app's private files directory
+                    path = reactContext.getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+                }
+            }
+
+            if (ReadableMapUtils.hasAndNotEmptyString(storageOptions, "path"))
+            {
+                path = new File(path, storageOptions.getString("path"));
+            }
+        } 
+        else {
+            if (forceLocal) 
+            {
+                path = reactContext.getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+            }
+        }
 
         File result = new File(path, filename);
 
