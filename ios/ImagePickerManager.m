18ee85b339bb11b85e58408c54e326d0e8ff5742
Andrei Tofan <tofan.andrei@gmail.com>
2020-07-06 20:43:30 +0300

    Make sure the calls are execute on the main thread (#1265)
    * Make sure the calls are execute on the main thread
    
    * Fix formatting
    
    * Wrap method calls
    
    Co-authored-by: Andrei Tofan <andrei@optinify.io>

--- ios/ImagePickerManager.m
+++ ios/ImagePickerManager.m
@@ -25,13 +25,17 @@ RCT_EXPORT_MODULE();
 RCT_EXPORT_METHOD(launchCamera:(NSDictionary *)options callback:(RCTResponseSenderBlock)callback)
 {
     self.callback = callback;
-    [self launchImagePicker:RNImagePickerTargetCamera options:options];
+    dispatch_async(dispatch_get_main_queue(), ^{
+        [self launchImagePicker:RNImagePickerTargetCamera options:options];
+    });
 }
 
 RCT_EXPORT_METHOD(launchImageLibrary:(NSDictionary *)options callback:(RCTResponseSenderBlock)callback)
 {
     self.callback = callback;
-    [self launchImagePicker:RNImagePickerTargetLibrarySingleImage options:options];
+    dispatch_async(dispatch_get_main_queue(), ^{
+        [self launchImagePicker:RNImagePickerTargetLibrarySingleImage options:options];
+    });
 }
 
 RCT_EXPORT_METHOD(showImagePicker:(NSDictionary *)options callback:(RCTResponseSenderBlock)callback)
