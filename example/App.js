a64bb922d47269c32768d91f6a52c2deb3222758
Raviraj <ivarjar22@yahoo.com>
2020-09-23 20:37:21 +0530

    Show Image component from returned URI in example

--- example/App.js
+++ example/App.js
@@ -3,7 +3,8 @@ import {
   StyleSheet,
   Text,
   View,
-  Button
+  Button,
+  Image
 } from 'react-native';
 import ImagePicker from '../src';
 
@@ -49,6 +50,13 @@ export default class App extends React.Component {
         />
 
         <Text>{JSON.stringify(this.state.response)}</Text>
+
+        {this.state.response && (
+          <Image
+            style={{width: 100, height: 100}}
+            source={{uri: this.state.response.uri}}
+          />
+        )}
       </View>
     );
   }
