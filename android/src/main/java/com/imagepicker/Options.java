73d84b895560cd228d4b09889c6e1a4f16a7e9b1
Johan du Toit <jdutoit.dev@gmail.com>
2020-12-21 14:43:19 +0200

    fix(video-quality): 🐛 map video quality to relevant int (#1549)

--- android/src/main/java/com/imagepicker/Options.java
+++ android/src/main/java/com/imagepicker/Options.java
@@ -1,11 +1,12 @@
 package com.imagepicker;
 
 import com.facebook.react.bridge.ReadableMap;
+import android.text.TextUtils;
 
 public class Options {
     Boolean pickVideo = false;
     Boolean includeBase64;
-    String videoQuality;
+    int videoQuality = 1;
     int quality;
     int maxWidth;
     int maxHeight;
@@ -18,7 +19,12 @@ public class Options {
             pickVideo = true;
         }
         includeBase64 = options.getBoolean("includeBase64");
-        videoQuality = options.getString("videoQuality");
+        
+        String videoQualityString = options.getString("videoQuality");
+        if(!TextUtils.isEmpty(videoQualityString) && !videoQualityString.toLowerCase().equals("high")) {
+            videoQuality = 0;
+        }
+
         quality = (int) (options.getDouble("quality") * 100);
         maxHeight = options.getInt("maxHeight");
         maxWidth = options.getInt("maxWidth");
