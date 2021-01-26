1a10a0ece6c7d710e37d7a10148220053661d90a
Gaspard Chevassus <chevassusgaspard@gmail.com>
2021-01-26 14:07:09 +0100

    fix

--- ios/ImagePickerManager.m
+++ ios/ImagePickerManager.m
@@ -101,7 +101,7 @@ RCT_EXPORT_METHOD(launchImageLibrary:(NSDictionary *)options callback:(RCTRespon
 
     NSURL *imagePath = [info objectForKey:UIImagePickerControllerReferenceURL];
     NSString *originalImagePath = imagePath.absoluteString;
-    PHFetchResult *result = [PHAsset fetchAssetsWithALAssetURLs:@[originalImagePath] options:nil];
+    PHFetchResult *result = [PHAsset fetchAssetsWithALAssetURLs:@[imagePath] options:nil];
     NSString *originalFileName = [[result firstObject] filename];
 
     NSURL *imageURL = [ImagePickerManager getNSURLFromInfo:info];
