85210f49f2b1e64b4800e580b1a2234b7e3b57e8
Simon Gourdeau <simon.gourdeau@hotmail.fr>
2021-06-29 09:18:51 +0200

    Fix gif returning as jpeg on android

--- android/src/main/java/com/imagepicker/Utils.java
+++ android/src/main/java/com/imagepicker/Utils.java
@@ -297,6 +297,7 @@ public class Utils {
         switch (mimeType) {
             case "image/jpeg": return "jpg";
             case "image/png": return "png";
+            case "image/gif": return "gif";
         }
         return "jpg";
     }
