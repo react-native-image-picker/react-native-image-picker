fe083ea745dd2bb046b6f05de4cd5dacf14cedc3
Chris Bobbe <cbobbe@zulip.com>
2021-12-10 17:52:10 -0800

    fix: Add `cookedFileName`, to give `fileName` with extension when possible
    Instead of changing `fileName` itself (for example, as in the
    not-merged PR #1881), since that would be a breaking change. Users
    can opt in to this improved logic on their own schedule, until we
    apply the new logic to `fileName` itself in a future major release.
    
    Empirically, before this change, `fileName` never has an extension
    when the file is a video on Android. It may be missing in some
    additional cases; such cases will also be covered, naturally.
    
    It doesn't seem natural that anyone would prefer a name without an
    extension over a name with one. Even if someone does, though, it's
    luckily easier for a user to write correct code to strip out an
    extension than to add one.
    
    `mime/lite` reportedly adds just ~2.5KB, and it doesn't bring in any
    of its own dependencies:
      https://www.npmjs.com/package/mime

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import {NativeModules} from 'react-native';
+import mime from 'mime/lite';
 
-import {CameraOptions, ImageLibraryOptions, Callback, ImagePickerResponse} from './types';
+import {Asset, CameraOptions, ImageLibraryOptions, Callback, ImagePickerResponse} from './types';
 export * from './types';
 
 const DEFAULT_OPTIONS: ImageLibraryOptions & CameraOptions = {
@@ -17,11 +18,44 @@ const DEFAULT_OPTIONS: ImageLibraryOptions & CameraOptions = {
   includeExtra: false,
 };
 
+function withCookedFileName(asset: Asset): Asset {
+  const unchanged = () => ({ ...asset, cookedFileName: asset.fileName });
+
+  const { type, fileName } = asset;
+  if (type == null || fileName == null) {
+    // Not enough data
+    // TODO: Can remove this condition if `Asset.type` and `Asset.fileName`
+    //   become non-optional.
+    return unchanged();
+  }
+  if (/.\..+$/.test(fileName)) {
+    // Already has an extension
+    return unchanged();
+  }
+  // An extension can't in general be extracted directly from a MIME-type
+  // string; we need a data source. See
+  //   https://github.com/react-native-image-picker/react-native-image-picker/pull/1881#issuecomment-992034446
+  //   https://www.npmjs.com/package/mime#user-content-lite-version
+  const extension = mime.getExtension(type);
+  if (extension == null) {
+    // Appropriate extension not found
+    return unchanged();
+  }
+  return {
+    ...asset,
+    cookedFileName: `${fileName}.${extension}`,
+  };
+}
+
 export function launchCamera(options: CameraOptions, callback?: Callback) : Promise<ImagePickerResponse> {
   return new Promise(resolve => {
     NativeModules.ImagePickerManager.launchCamera(
       {...DEFAULT_OPTIONS, ...options},
-      (result: ImagePickerResponse) => {
+      (rawResult: ImagePickerResponse) => {
+        const result = {
+          ...rawResult,
+          assets: rawResult.assets?.map(asset => withCookedFileName(asset)),
+        };
         if(callback) callback(result);
         resolve(result);
       },
@@ -36,7 +70,11 @@ export function launchImageLibrary(
   return new Promise(resolve => {
     NativeModules.ImagePickerManager.launchImageLibrary(
       {...DEFAULT_OPTIONS, ...options},
-      (result: ImagePickerResponse) => {
+      (rawResult: ImagePickerResponse) => {
+        const result = {
+          ...rawResult,
+          assets: rawResult.assets?.map(asset => withCookedFileName(asset)),
+        };
         if(callback) callback(result);
         resolve(result);
       },
