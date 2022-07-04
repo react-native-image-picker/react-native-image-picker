57a0516ad4dd937e4952b232eb9e93f48c389ee6
Artur Yorsh <artyorsh.me@gmail.com>
2022-07-04 19:33:05 +0200

    fix(android): VideoMetadata to be compatible with Android SDK33

--- android/src/main/java/com/imagepicker/VideoMetadata.java
+++ android/src/main/java/com/imagepicker/VideoMetadata.java
@@ -43,7 +43,11 @@ public class VideoMetadata extends Metadata {
       this.height = bitmap.getHeight();
     }
 
-    metadataRetriever.release();
+    try {
+      metadataRetriever.release();
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
   }
 
   public int getBitrate() {
