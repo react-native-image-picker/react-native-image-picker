6b9a2b8b046530b992aaff5605340d5bf083451d
Jakub Piasecki <jakub.piasecki@swmansion.com>
2023-02-14 15:45:26 +0100

    Update example app to RN 0.71

--- example/babel.config.js
+++ example/babel.config.js
@@ -3,14 +3,4 @@ const pak = require('../package.json');
 
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
-  plugins: [
-    [
-      'module-resolver',
-      {
-        alias: {
-          [pak.name]: path.join(__dirname, '..', pak.source),
-        },
-      },
-    ],
-  ],
 };
