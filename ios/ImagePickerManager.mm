73330e7189aa8cbcead72581ccefcefb9e11e713
Nishit Rathi <77700933+ratz6@users.noreply.github.com>
2023-05-30 14:31:54 +0530

    Update ImagePickerManager.mm

--- ios/ImagePickerManager.mm
+++ ios/ImagePickerManager.mm
@@ -262,7 +262,7 @@ CGImagePropertyOrientation CGImagePropertyOrientationForUIImageOrientation(UIIma
     
     NSMutableDictionary *response = [[NSMutableDictionary alloc] init];
     
-    if([self.options[@"formatAsMp4"] boolValue] && ![fileExtension isEqualToString:@"mp4"])  ) {
+    if([self.options[@"formatAsMp4"] boolValue] && ![fileExtension isEqualToString:@"mp4"]) {
         NSURL *parentURL = [videoDestinationURL URLByDeletingLastPathComponent];
         NSString *path = [[parentURL.path stringByAppendingString:@"/"] stringByAppendingString:[[NSUUID UUID] UUIDString]];
         path = [path stringByAppendingString:@".mp4"];
