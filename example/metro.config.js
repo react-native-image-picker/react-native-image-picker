2b0880e43da54c6faddc3de5bf4ef56d2e6f2409
Gabriel Donadel <donadeldev@gmail.com>
2024-03-13 12:33:18 -0300

    chore(example): Bump react-native to 0.73.6

--- example/metro.config.js
+++ example/metro.config.js
@@ -1,14 +1,12 @@
 const path = require('path');
-
-module.exports = {
-  transformer: {
-    getTransformOptions: async () => ({
-      transform: {
-        experimentalImportSupport: false,
-        inlineRequires: true,
-      },
-    }),
-  },
+const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
+/**
+ * Metro configuration
+ * https://facebook.github.io/metro/docs/configuration
+ *
+ * @type {import('metro-config').MetroConfig}
+ */
+const config = {
   resolver: {
     extraNodeModules: new Proxy(
       {},
@@ -17,3 +15,5 @@ module.exports = {
   },
   watchFolders: [path.resolve('.'), path.resolve('..')],
 };
+
+module.exports = mergeConfig(getDefaultConfig(__dirname), config);
