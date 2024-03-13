e6b39736f0598423ae17f19e449493f22966e4d2
Gabriel Donadel Dall'Agnol <donadeldev@gmail.com>
2024-03-13 14:21:18 -0300

    chore(example): Bump react-native to 0.73.6 (#2283)

--- example/babel.config.js
+++ example/babel.config.js
@@ -1,6 +1,3 @@
-const path = require('path');
-const pak = require('../package.json');
-
 module.exports = {
-  presets: ['module:metro-react-native-babel-preset'],
+  presets: ['module:@react-native/babel-preset'],
 };
